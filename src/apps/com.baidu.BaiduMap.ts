c4cd0dfe1b9fd6816e783f6674e8eee59cde3ef5
Tyllllll <874026556@qq.com>
2024-06-20 15:59:46 +0800

    test: add a baudimap rule

---
+++ src/apps/com.baidu.BaiduMap.ts
@@ -0,0 +1,19 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.baidu.BaiduMap',
+  name: '百度地图',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-地图上方横幅',
+      quickFind: true,
+      rules: [
+        {
+          activityIds: 'com.baidu.baidumaps.MapsActivity',
+          matches: 'ImageView[id="com.baidu.BaiduMap:id/yellow_banner_close"]',
+        },
+      ],
+    },
+  ],
+});
