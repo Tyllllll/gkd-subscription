c0893838b832e9c00995a7eefa8ad7322e08e718
Tyllllll <874026556@qq.com>
2024-06-26 10:41:29 +0800

    chore: 百度地图更新实体限定

--- src/apps/com.baidu.BaiduMap.ts
+++ src/apps/com.baidu.BaiduMap.ts
@@ -18,6 +18,7 @@ export default defineGkdApp({
       quickFind: true,
       matchTime: 5000,
       actionMaximum: 1,
+      activityIds: 'com.baidu.baidumaps.MapsActivity',
       resetMatch: 'app',
       rules: 'ImageView[id="com.baidu.BaiduMap:id/cancel_update"]',
       snapshotUrls: 'https://i.gkd.li/i/12909385',
