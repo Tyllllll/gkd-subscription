5f76f96db1eca88ebf38f76a5e2e0cc8fd479c87
Tyllllll <874026556@qq.com>
2024-06-28 16:34:25 +0800

    feat: 添加微博国际版热推规则

--- src/apps/com.sina.weibo.ts
+++
@@ -1,7 +0,0 @@
-import { defineGkdApp } from '@gkd-kit/define';
-
-export default defineGkdApp({
-  id: 'com.sina.weibo',
-  name: '微博',
-  groups: [],
-});
---
+++ src/apps/com.weico.international.ts
@@ -0,0 +1,34 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.weico.international',
+  name: '微博轻享版',
+  groups: [
+    {
+      key: 0,
+      name: '局部广告-微博热推',
+      activityIds: [
+        'com.weico.international.activity.MainFragmentActivity',
+        'com.weico.international.ui.search.SearchActivity',
+      ],
+      quickFind: true,
+      actionCd: 10000,
+      rules: [
+        {
+          key: 0,
+          matches: 'TextView[text="热推"] +n ImageView[desc="关闭广告"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/16026346',
+            'https://i.gkd.li/i/16026365',
+          ],
+        },
+        {
+          key: 1,
+          preKeys: [0],
+          matches: 'TextView[text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/i/16026402',
+        },
+      ],
+    },
+  ],
+});
