ee8d3e4733d058f98c32a88f359a506a4f91e638
Tyllllll <874026556@qq.com>
2024-06-29 20:44:09 +0800

    test: 测试番茄缓慢查询

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -8,12 +8,12 @@ export default defineGkdApp({
       key: 0,
       name: '局部广告-小说底部广告',
       actionCd: 25000, // 猜的时间
-      quickFind: true,
-      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
       rules: [
         {
           key: 0,
           name: '点击广告箭头',
+          quickFind: true,
+          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
           matches: 'FrameLayout[id="com.dragon.read:id/cv_"] > ImageView',
           snapshotUrls: 'https://i.gkd.li/i/15922031',
         },
@@ -21,6 +21,8 @@ export default defineGkdApp({
           key: 1,
           name: '点击关闭广告',
           matches: 'TextView[text="关闭此条广告"]',
+          quickFind: true,
+          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
           snapshotUrls: 'https://i.gkd.li/i/15922175',
           preKeys: [0],
         },
